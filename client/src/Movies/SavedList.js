fee6cf084399b4c88770325759a68fd00f297853
Eric Richards <esholundr@yahoo.com>
2019-09-17 21:57:16 -0600

    Fixed the home button on SavedList, mvp complete

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-const SavedList = props => (
-	<div className="saved-list">
-		<h3>Saved Movies:</h3>
-		{props.list.map(movie => (
-			<span className="saved-movie">{movie.title}</span>
-		))}
-		<div className="home-button">Home</div>
-	</div>
-);
+function SavedList(props) {
+	const routeToHome = event => {
+		event.preventDefault();
+		props.history.push("/");
+	};
 
+	return (
+		<div className="saved-list">
+			<h3>Saved Movies:</h3>
+			{props.list.map(movie => (
+				<span className="saved-movie">{movie.title}</span>
+			))}
+			<button onClick={routeToHome} className="home-button">
+				Home
+			</button>
+		</div>
+	);
+}
 export default SavedList;
